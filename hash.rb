dffbda87bb43b7690d4d8e0d3acd93f982a9ae29
Augusto Omena <augusto.omena.n@gmail.com>
2024-02-06 21:16:15 -0300

    Códigos Ruby

---
+++ hash.rb
@@ -0,0 +1,9 @@
+pessoa = {
+  "nome" => "João",
+  "idade" => 30,
+  "profissao" => "Desenvolvedor"
+}
+
+puts "Nome: #{pessoa["nome"]}"
+puts "Idade: #{pessoa["idade"]}"
+puts "Profissão: #{pessoa["profissao"]}"
---
+++ livros.rb
@@ -0,0 +1,14 @@
+def livros()
+    livros = ["Código limpo", "Quem pensa enriquece", "Hábitos Atômicos"]
+    return livros
+end
+
+def ehigual(str, str2)
+    return (str == str2)
+end
+
+
+Ehigual = ehigual("oi", "ol")
+
+puts livros
+puts Ehigual
