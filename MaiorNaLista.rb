02b9f8e5e365718ed8d4d95d5506cda1e6d09eaf
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2024-02-22 13:03:19 +0000

    Práticando listas em ruby.

---
+++ MaiorNaLista.rb
@@ -0,0 +1,12 @@
+listNumeros = [1, 2, 3, 4, 70, 30, 20, 10]
+
+maior_numero = listNumeros.max
+menor_numero = listNumeros.min
+lista_ordenada = listNumeros.sort
+lista_invertida = listNumeros.sort.reverse
+
+
+puts "O maior numero dentro da lista é #{maior_numero}"
+puts "O menor numero dentro da lista é #{menor_numero}"
+puts "A lista ordenada fica assim: #{lista_ordenada}"
+puts "A lista invertida fica assim: #{lista_invertida}"
---
+++ operadores.rb
@@ -0,0 +1,15 @@
+puts "Digite um valor"
+
+a = gets.chomp.to_i
+
+puts "Digite outro valor"
+
+b = gets.chomp.to_i
+
+puts " "
+puts a + b
+puts a - b
+puts a * b
+puts a / b
+puts a % b
+puts a ** b
